0766a379b6336aea22e27001f5ade44bc696bec3
Kosmas Chatzimichalis <kosmas@mach7x.com>
2017-06-10 17:29:17 +0200

    D1 - Finding a Cart

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :carts
   root 'store#index', as: 'store_index'
 
   resources :products
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170530120133) do
+ActiveRecord::Schema.define(version: 20170610152025) do
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
