e3ab97ea7b03624228653c1ad6541ca6d21fa91f
Kosmas Chatzimichalis <kosmas@mach7x.com>
2017-06-20 18:53:35 +0200

    F2 - Creating an Ajax-Based Cart

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -32,6 +32,7 @@ class LineItemsController < ApplicationController
     respond_to do |format|
       if @line_item.save
         format.html { redirect_to store_index_url }
+        format.js
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new }
