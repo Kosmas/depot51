9bf23482793d687f7587d46c918ac9e429d46a9d
Kosmas Chatzimichalis <kosmas@mach7x.com>
2017-06-20 18:23:17 +0200

    F1 - Moving the Cart

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -31,7 +31,7 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart }
+        format.html { redirect_to store_index_url }
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new }
