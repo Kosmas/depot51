04b9323353e04778c3a102e2038b05b1ce80f22e
Kosmas Chatzimichalis <kosmas@mach7x.com>
2017-06-24 17:30:32 +0200

    F4 - Hiding an Empty Cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -58,7 +58,7 @@ class CartsController < ApplicationController
     @cart.destroy if @cart.id == session[:cart_id]
     session[:cart_id] = nil
     respond_to do |format|
-      format.html { redirect_to store_index_url, notice: 'Your cart is currently empty' }
+      format.html { redirect_to store_index_url }
       format.json { head :no_content }
     end
   end
