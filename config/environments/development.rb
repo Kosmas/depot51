e34269583e338e5bd4ad979f4f3203bb1365ba94
Kosmas Chatzimichalis <kosmas@mach7x.com>
2017-08-26 18:43:27 +0200

    B6.0 - update for C iteration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,6 +14,7 @@ Rails.application.configure do
 
   # Enable/disable caching. By default caching is disabled.
   if Rails.root.join('tmp/caching-dev.txt').exist?
+    config.action_controller.enable_fragment_cache_logging = true
     config.action_controller.perform_caching = true
 
     config.cache_store = :memory_store
--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -4,10 +4,9 @@ class StoreControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get store_index_url
     assert_response :success
-    assert_select '#columns #side a', minimum: 4
-    assert_select '#main .entry', 3
-    assert_select 'h3', 'Programming Ruby 1.9'
+    assert_select 'nav.side_nav a', minimum: 4
+    assert_select 'main ul.catalog li', 3
+    assert_select 'h2', 'Programming Ruby 1.9'
     assert_select '.price', /\$[,\d]+\.\d\d/
   end
-
 end
