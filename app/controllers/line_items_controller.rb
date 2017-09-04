a40ef170533f84dbbfe008a0a3b78cbbbdaf43fd
Kosmas Chatzimichalis <kosmas@mach7x.com>
2017-09-04 19:07:45 +0200

    B6.0 - update for E iteration

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -27,11 +27,11 @@ class LineItemsController < ApplicationController
   # POST /line_items.json
   def create
     product = Product.find(params[:product_id])
-    @line_item = @cart.line_items.build(product: product)
+    @line_item = @cart.add_product(product)
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart, notice: 'Line item was successfully created.' }
+        format.html { redirect_to @line_item.cart }
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new }
