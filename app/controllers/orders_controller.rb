ffeef767d8d50c85a726a578a0ac9b5f42c8e4b3
Kosmas Chatzimichalis <kosmas@mach7x.com>
2017-10-13 18:09:09 +0200

    I1 - Sending Confirmation Emails

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -34,6 +34,7 @@ class OrdersController < ApplicationController
       if @order.save
         Cart.destroy(session[:cart_id])
         session[:cart_id] = nil
+        OrderMailer.received(@order).deliver_later
         format.html { redirect_to store_index_url, notice: 'Thank you for your order.' }
         format.json { render :show, status: :created, location: @order }
       else
