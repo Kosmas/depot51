9b2b6b8265dafa25997bff14f94aff1eaba7f2fb
Kosmas Chatzimichalis <kosmas@mach7x.com>
2017-10-13 17:29:22 +0200

    H2 - Testing our JavaScript Functionality

--- test/system/orders_test.rb
+++ test/system/orders_test.rb
@@ -1,9 +1,21 @@
 require "application_system_test_case"
 
 class OrdersTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit orders_url
-  #
-  #   assert_selector "h1", text: "Order"
-  # end
+  test "check routing number" do
+    visit store_index_url
+
+    first('.catalog li').click_on 'Add to Cart'
+
+    click_on 'Checkout'
+
+    fill_in 'order_name', with: 'Dave Thomas'
+    fill_in 'order_address', with: '123 Main Street'
+    fill_in 'order_email', with: 'dave@example.com'
+
+    assert_no_selector "#order_routing_number"
+
+    select 'Check', from: 'pay_type'
+
+    assert_selector "#order_routing_number"
+  end
 end
