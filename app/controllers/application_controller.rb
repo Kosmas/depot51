29f1c3f79150ae978f56b559222fab30638fd98f
Kosmas Chatzimichalis <kosmas@mach7x.com>
2017-11-06 17:28:09 +0100

    K1 - Selecting the Locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,21 @@
 class ApplicationController < ActionController::Base
+  before_action :set_i18n_locale_from_params
   before_action :authorize
 
   protect_from_forgery with: :exception
 
   protected
+
+  def set_i18n_locale_from_params
+    if params[:locale]
+      if I18n.available_locales.map(&:to_s).include?(params[:locale])
+        I18n.locale = params[:locale]
+      else
+        flash.now[:notice] = "#{params[:locale]} translation not available"
+        logger.error flash.now[:notice]
+      end
+    end
+  end
 
   def authorize
     unless User.find_by(id: session[:user_id])
--- config/routes.rb
+++ config/routes.rb
@@ -8,14 +8,16 @@ Rails.application.routes.draw do
   end
 
   resources :users
-  resources :orders
-  resources :line_items
-  resources :carts
-  root 'store#index', as: 'store_index'
 
   resources :products do
     get :who_bought, on: :member
   end
 
+  scope '(:locale)' do
+    resources :orders
+    resources :line_items
+    resources :carts
+    root 'store#index', as: 'store_index', via: :all
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
