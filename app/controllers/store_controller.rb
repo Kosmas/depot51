7b325d22b55baede8803098f7ab269118a598af2
Kosmas Chatzimichalis <kosmas@mach7x.com>
2017-11-28 16:29:20 +0100

    K4 - Adding a Locale Switcher

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -4,6 +4,10 @@ class StoreController < ApplicationController
   include CurrentCart
   before_action :set_cart
   def index
-    @products = Product.order(:title)
+    if params[:set_locale]
+      redirect_to store_index_url(locale: params[:set_locale])
+    else
+      @products = Product.order(:title)
+    end
   end
 end
